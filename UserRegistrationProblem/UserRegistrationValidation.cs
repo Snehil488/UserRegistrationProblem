c9eba4780394cee0a4c37d60db0a42da73c399af
Snehil <snehilverma488@gmail.com>
2020-10-06 12:08:03 +0530

    [Snehil] Add : Validating Last Name Of User

--- UserRegistrationProblem/UserRegistrationValidation.cs
+++ UserRegistrationProblem/UserRegistrationValidation.cs
@@ -8,9 +8,14 @@ namespace UserRegistrationProblem
     class UserRegistrationValidation
     {
         private static string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
+        private static string REGEX_LASTNAME = "^[A-Z][a-z]{2,}$";
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }    
+        public bool ValidateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, REGEX_LASTNAME);
         }
         public void PrintResult(bool result)
         {
